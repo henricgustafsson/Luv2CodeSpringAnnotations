0bfcd53c3ef70bf4c3e2c073a0e3c15490a4f30a
henricgustafsson <50734521+henricgustafsson@users.noreply.github.com>
2020-01-13 18:16:52 +0100

    pom_spring_5.0.6_added_added_interface_coach_added_class_tennischaoch

---
+++ spring-demo-annotations/src/main/java/se/lexicon/henric/spring_demo_annotations/Coach.java
@@ -0,0 +1,9 @@
+package se.lexicon.henric.spring_demo_annotations;
+import org.springframework.stereotype.Component;
+
+@Component 
+public interface Coach {
+
+	public String GetDailyWorkout();
+	
+}
---
+++ spring-demo-annotations/src/main/java/se/lexicon/henric/spring_demo_annotations/TennisCoach.java
@@ -0,0 +1,11 @@
+package se.lexicon.henric.spring_demo_annotations;
+
+
+public class TennisCoach implements Coach {
+
+	public String GetDailyWorkout() {
+		
+		return "Practice your backhand volley";
+	}
+
+}
