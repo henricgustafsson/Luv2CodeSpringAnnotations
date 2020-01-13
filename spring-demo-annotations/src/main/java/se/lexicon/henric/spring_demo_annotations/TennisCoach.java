b8d6839c50fdc8cbe2d87f398ee099685a1ee953
henricgustafsson <50734521+henricgustafsson@users.noreply.github.com>
2020-01-13 21:45:05 +0100

    Refactor_Tenniscoach_App_Using_Setter_injection

--- spring-demo-annotations/src/main/java/se/lexicon/henric/spring_demo_annotations/TennisCoach.java
+++ spring-demo-annotations/src/main/java/se/lexicon/henric/spring_demo_annotations/TennisCoach.java
@@ -8,14 +8,24 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
+	public TennisCoach() {
+		//default constructor
 	}
+	
+	// using setter injection instead of constructor
+	// injection
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
+	
 	public String getDailyWorkout() {
 		
 		return "Practice your backhand volley";
 	}
+
+	
 
 	@Override
 	public String getDailyFortune() {
@@ -23,4 +33,9 @@ public class TennisCoach implements Coach {
 		return fortuneService.getFortune();
 	}
 
+	//setter for field FortuneService fortuneService
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 }
